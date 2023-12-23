d2baf5bfda17a9bac23313beb241bb2601305b46
rayanebait <rayane-bait@outlook.fr>
2023-12-23 16:54:00 +0100

    last vers

---
+++ crates/libs/lib-network/src/handle_action.rs
@@ -0,0 +1,84 @@
+use std::net::UdpSocket;
+use std::sync::{Arc, Mutex};
+
+use crate::packet::{Packet, PacketBuilder};
+use crate::handle_packet::HandlingError;
+use crate::action::Action;
+
+use crate::congestion_handler::*;
+
+
+/*Waits for the signal that the action queue is not empty 
+then handles the action. Can push to the send queue so
+it also notifies the send queue wether is it empty or not. */
+pub async fn handle_action_task(send_queue: Arc<Mutex<SendQueue>>,
+                                send_queue_state: Arc<QueueState>,
+                                action_queue: Arc<Mutex<ActionQueue>>,
+                                action_queue_state: Arc<QueueState>){
+    tokio::spawn(async move {
+        loop {
+            match ActionQueue::lock_and_pop(Arc::clone(&action_queue)){
+                Some(action)=> {
+                    /*action queue is not empty get an action and handle it*/
+                    println!("handle action");
+                    QueueState::set_non_empty_queue(Arc::clone(&send_queue_state));
+                    handle_action(action,
+                        Arc::clone(&send_queue),
+                    Arc::clone(&send_queue_state)
+                        /*return the action required */
+                    )},
+                None=>{
+                    /*
+                        action queue is empty wait for the activity of 
+                        the receive queue
+                        */
+                    
+                    println!("action wait");
+                    QueueState::set_empty_queue(Arc::clone(&action_queue_state));
+                    action_queue_state.wait();
+                    continue
+                }
+            };
+        }
+    }).await;
+}
+
+pub fn handle_action(action: Action, send_queue: Arc<Mutex<SendQueue>>, send_queue_state: Arc<QueueState>){
+    match action {
+        Action::NoOp(sock_addr) =>{
+            println!("Received NoOp packet from {}\n", sock_addr);
+            return;
+        }, 
+        Action::ProcessDatum(..) =>(), 
+        Action::ProcessErrorReply(..) =>(), 
+        Action::ProcessHelloReply(..) =>(), 
+        
+        Action::SendHelloReply(id, sock_addr)=>{
+            let packet = PacketBuilder::hello_reply_packet(&id);
+            SendQueue::lock_and_push(Arc::clone(&send_queue), packet, sock_addr);
+            QueueState::set_non_empty_queue(Arc::clone(&send_queue_state));
+        },
+        Action::SendError(..) =>(), 
+        Action::SendDatumWithHash(..) =>(), 
+        Action::SendPublicKey(optional_key, sock_addr) =>{
+            let packet = PacketBuilder::public_key_packet(optional_key);
+            SendQueue::lock_and_push(Arc::clone(&send_queue), packet, sock_addr);
+            QueueState::set_non_empty_queue(Arc::clone(&send_queue_state));
+        }, 
+        Action::SendRoot(optional_root, sock_addr) =>{
+            let packet = PacketBuilder::root_packet(optional_root);
+            SendQueue::lock_and_push(Arc::clone(&send_queue), packet, sock_addr);
+            QueueState::set_non_empty_queue(Arc::clone(&send_queue_state));
+        },
+
+        Action::SendRootReply(optional_root, sock_addr) =>{
+            let packet = PacketBuilder::root_reply_packet(optional_root);
+            SendQueue::lock_and_push(Arc::clone(&send_queue), packet, sock_addr);
+            QueueState::set_non_empty_queue(Arc::clone(&send_queue_state));
+        },
+
+        Action::StoreRoot(..) =>(), 
+        Action::StorePublicKey(..) =>(), 
+        _ =>(),
+    };
+}
---
+++ crates/libs/lib-network/src/sender_receiver.rs
@@ -0,0 +1,76 @@
+use std::sync::{Arc, Mutex, PoisonError, Condvar};
+use std::thread::sleep;
+use tokio::{net::UdpSocket, spawn};
+use tokio_util::task::TaskTracker;
+
+use crate::congestion_handler::{ReceiveQueue, SendQueue, QueueState, PendingIds};
+use crate::packet::{Packet, PacketError};
+
+/*
+    Maybe lock only first and last element ? sender accesses only the first
+    handle action accesses only the last 
+*/
+pub async fn receiver(sock: Arc<UdpSocket>,
+         receive_queue: Arc<Mutex<ReceiveQueue>>,
+         receive_queue_state: Arc<QueueState>){
+    tokio::spawn(async move {
+        loop {
+            /*Get the first packet in the Queue or None if the queue 
+            is empty */
+            let (packet, sock_addr) =
+            match Packet::recv_from(&sock).await{
+                Ok(packet_and_addr) => packet_and_addr,
+                _=> continue,
+            };
+            println!("receiver received");
+
+            /*Maybe should be async? */
+            ReceiveQueue::lock_and_push(
+                    Arc::clone(&receive_queue),
+                        sock_addr, packet);
+
+            QueueState::set_non_empty_queue(Arc::clone(&receive_queue_state));
+
+        };
+    }).await;
+}
+
+pub async fn sender(sock: Arc<UdpSocket>,
+         send_queue: Arc<Mutex<SendQueue>>,
+         send_queue_state: Arc<QueueState>,
+         pending_ids_to_add: Arc<Mutex<PendingIds>>){
+    tokio::spawn(async move {
+        loop {
+            /*Get the first packet in the Queue or None if the queue 
+            is empty */
+            let packet_for_addr ={
+                let mut guard  = match send_queue.lock() {
+                    Ok(guard) => guard,
+                    Err(poison_error) =>
+                                    panic!("Poisoned Mutex found in sender"),
+                };
+
+                /*pops the front packet if it exists */
+                guard.get_packet()
+            };
+
+            /*If the queue is empty, put the thread to sleep until queue 
+            is not empty*/
+            let (packet, sock_addr) = match packet_for_addr {
+                Some(packet_for_addr) => packet_for_addr,
+                None=> {
+                    println!("sender wait");
+                    QueueState::set_empty_queue(Arc::clone(&send_queue_state));
+                    send_queue_state.wait();
+                    // sleep(std::time::Duration::from_secs(1));
+                    continue
+                },
+            };
+
+            PendingIds::lock_and_add_packet_id_raw(Arc::clone(&pending_ids_to_add),
+                                 packet.get_id(), &sock_addr);
+            packet.send_to_addr(&sock, &sock_addr).await.unwrap();
+            println!("sender sent");
+        };
+    }).await;
+}
