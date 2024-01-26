b160e8545a74a71ae66cdc76600d8a308d93f59b
rayanebait <rayane-bait@outlook.fr>
2024-01-26 15:56:12 +0100

    a

--- crates/libs/lib-network/src/congestion_handler.rs
+++ crates/libs/lib-network/src/congestion_handler.rs
@@ -506,7 +506,7 @@ impl PendingIds {
             pending_ids_guard.id_to_addr.iter_mut()
         {
             match *attempts {
-                3 | 5 | 7=>{
+                3 | 5 | 7 | 9=>{
                     *attempts+=1;
                     id_to_send_nat_trav.push(*id);
                     id_to_resend.push(*id);
